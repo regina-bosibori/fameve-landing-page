6fd1c1d1330c4828579dba33afc4c7d26fbe2dc7
regina-bosibori <msomegina@gmail.com>
2024-03-22 16:21:40 +0300

    home page update

---
+++ index.js
@@ -0,0 +1,17 @@
+// const emailField=document.getElementById("email");
+// const emailError=document.getElementById("email-error")
+// const emailForm=document.getElementById("emailform")
+
+// function validateEmail(){
+//     if (emailField.value===null){
+//         emailError.innerHTML="Please enter email"
+//     }
+// }
+// emailForm.addEventListener("submit",(e)=>{
+//     let messages=[]
+//     if(emailField.value===''){
+//         messages.push('Email is required')
+//     }
+//     emailError.innerHTML=messages.
+
+// } )
